1db4a783638cf41a444f4aea5beb3de3c9123b06
robinmahto <robinmahto413@gmail.com>
2026-06-02 23:03:28 +0530

    build login API with all possible validation, JWT, cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { connectDatabase } from "./config/database.js";
 import UserModel from "./models/users.model.js";
 import { userValidation } from "./utils/userValidation.js";
@@ -13,7 +14,8 @@ app.get("/", (req, res) => {
   res.send("welcome back to syncdev");
 });
 
-app.post("/users", async (req, res) => {
+// signup
+app.post("/signup", async (req, res) => {
   try {
     // validate body
     const { error, value } = userValidation.validate(req.body);
@@ -38,6 +40,44 @@ app.post("/users", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
+      message: error.message,
+    });
+  }
+});
+
+// login
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // check email id is exist.
+    const users = await UserModel.findOne({ email }).select("+password");
+    if (!users) {
+      throw new Error("Invalid credentials");
+    }
+    // validate password using bcryptjs
+    const isPasswordCorrect = await bcrypt.compare(password, users.password);
+    if (!isPasswordCorrect) throw new Error("Invalid credentials");
+    // generate tokens
+    const token = jwt.sign({ _id: users._id }, process.env.JWT_SECERET, {
+      expiresIn: "1d",
+    });
+    // set to cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+    });
+    // send response
+    res.status(200).json({
+      status: true,
+      data: {
+        _id: users._id,
+        email: users.email,
+        name: `${users.firstName} ${users.lastName}`,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
       message: error.message,
     });
   }
--- readme.md
+++ readme.md
@@ -7,3 +7,18 @@
 - setup Models for users
 - data sanitizations and schema validations
 - Encrypting passwords
+
+### signup
+
+- validate payload using joi.
+- check if users already exits.
+- error handling
+- hash passowrd.
+- save data into db.
+
+### login
+
+- find emailId from database
+- validate password using bcryptjs
+- geneate JWT tokens
+- send tokens to cookies in response
